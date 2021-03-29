29ff4aead9535af990cdb5ce79ddab00bedbb8ca
davidlidebrandt <david.lidebrandt@gmail.com>
2021-03-29 13:43:47 +0000

    Create footer

--- fitness_equipment/settings.py
+++ fitness_equipment/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*v&-t(6_++(fqp%6_+mmxav(g1f)1h$w@bwyo$*y*u4=q41cd3'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

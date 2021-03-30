9d4d853d292efac51a8f0f374fe56a447e70decf
davidlidebrandt <david.lidebrandt@gmail.com>
2021-03-30 14:09:09 +0000

    Begin styling login/sign up form

--- fitness_equipment/settings.py
+++ fitness_equipment/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'fitness_equipment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

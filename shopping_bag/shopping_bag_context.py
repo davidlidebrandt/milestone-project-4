c40bbfb3c188a48059ec2f197b693a690cfda87c
davidlidebrandt <david.lidebrandt@gmail.com>
2021-04-09 14:48:39 +0000

    Add basic checkout template

--- shopping_bag/shopping_bag_context.py
+++ shopping_bag/shopping_bag_context.py
@@ -1,10 +1,13 @@
 from products.models import Product
+from django.shortcuts import get_object_or_404
 
 
 def bag(request):
-    id = 1
-    quantity = 1
-    return {
-        "product": Product.objects.get(id=id),
-        "quantity": quantity,
-    }
+    bag = request.session.get("shopping_bag", {})
+    context = {}
+    for product in bag:
+        if not product == "total_cost":
+            context[product] = {"product": get_object_or_404(
+                Product, id=product), "quantity": bag[product]["quantity"]}
+    print(context)
+    return {"context": context}
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -7,12 +7,16 @@ def checkout(request):
 
 
 def add_to_bag(request, id):
-    get_object_or_404(Product, id=id)
+    product = get_object_or_404(Product, id=id)
     bag = request.session.get("shopping_bag", {})
     if str(id) in bag.keys():
         bag[str(id)]["quantity"] += int(request.POST.get("quantity-input"))
     else:
         bag.update({id: {"quantity": int(request.POST.get("quantity-input"))}})
+    if "total_cost" in bag.keys():
+        bag["total_cost"] += product.prize
+    else:
+        bag["total_cost"] = product.prize
     request.session["shopping_bag"] = bag
     print(request.session["shopping_bag"])
     return redirect("checkout")

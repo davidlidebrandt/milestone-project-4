03b7f058f02ca48c915a5790dda474843184f422
davidlidebrandt <david.lidebrandt@gmail.com>
2021-04-17 15:01:35 +0000

    Create order/order items models

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             null=True, blank=True)
+    amount_paid = models.IntegerField()
+
+    def __str__(self):
+        return self.date
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product

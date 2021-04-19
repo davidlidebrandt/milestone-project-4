2f9bfc95bc56151cd140937c3b91b263f1628a96
davidlidebrandt <david.lidebrandt@gmail.com>
2021-04-19 11:38:04 +0000

    Add basic function for saving orders

--- payment/views.py
+++ payment/views.py
@@ -3,8 +3,11 @@ from django.conf import settings
 from django.http.response import JsonResponse
 from django.core.mail import send_mail
 from django.views.decorators.csrf import csrf_exempt
-from products.models import Product
+from django.contrib.auth.models import User
+from products.models import Product, Order, OrderItem
 import stripe
+import datetime
+import uuid
 
 
 # The Stripe docs and another tutotial was uses as the base for this code.
@@ -14,9 +17,12 @@ import stripe
 def create_checkout(request):
     if request.method == "POST":
         customer_email = ""
+        user_id = None
         if request.user.is_authenticated:
             customer_email = request.user.email
-        meta_data = {}
+            user_id = request.user.id
+        current_datetime = datetime.datetime.now()
+        meta_data = {"date": current_datetime, "user_id": user_id}
         line_items = []
         bag = request.session.get("shopping_bag")
         bag.pop("total_cost")
@@ -32,14 +38,11 @@ def create_checkout(request):
                 else:
                     for value in product_field.values():
                         product["quantity"] = value
-            meta_data["Product" + str(count)] = db_product.id
+            meta_data["products"]["product" + str(count)] = db_product.id
             count += 1
             line_items.append(product)
         stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
         try:
-            stripe.Customer.create(
-                description="My First Test Customer (created for API docs)",
-)
             checkout_session = stripe.checkout.Session.create(
                 success_url="https://fitness-equipment.herokuapp.com/payment/success/",
                 cancel_url="https://fitness-equipment.herokuapp.com/payment/error/",
@@ -80,10 +83,29 @@ def confirmation_of_payment(request):
             "Your order",
             "Your order was successful",
             None,
-            ["dl_brd@hotmail.com"],
+            [event["data"]["object"]["customer_email"]],
             fail_silently=False,)
+        order_id = uuid.uuid4
+        user_id = event["data"]["object"]["metadata"]["user_id"]
+        order = None
+        if user_id:
+            user = get_object_or_404(User, id=user_id)
+            order = Order(id=order_id,
+                          date=event["data"]["object"]["metadata"]["date"],
+                          user=user, amount_paid=event["data"]["object"]["amount_total"],
+                          shipping_address=event["data"]["object"]["shipping"]["address"],
+                          customer_name=event["data"]["object"]["customer_email"],
+                          customer_email=event["data"]["object"]["shipping"]["name"])
+        else:
+            order = Order(id=order_id,
+                          date=event["data"]["object"]["metadata"]["date"],
+                          amount_paid=event["data"]["object"]["amount_total"],
+                          shipping_address=event["data"]["object"]["shipping"]["address"],
+                          customer_name=event["data"]["object"]["customer_email"],
+                          customer_email=event["data"]["object"]["shipping"]["name"])
+        order.save()
 
-    return HttpResponse(status=200, content=event["data"]["object"]["customer_email"])
+    return HttpResponse(status=200)
 
 
 def payment_success(request):

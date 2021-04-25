e67e110b004f69db54f6f0879dba8edfb84b929c
davidlidebrandt <david.lidebrandt@gmail.com>
2021-04-25 18:25:56 +0000

    Fix faulty product test

--- products/test_views.py
+++ products/test_views.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.shortcuts import redirect, get_object_or_404
 from . models import Product, Discount, Category, Manufacturer
 
 
@@ -12,20 +11,23 @@ class TestViews(TestCase):
 
     def test_get_single_product_page(self):
         test_category = Category(name="Kettlebells")
+        test_category.save()
         test_discount = Discount(name="30%", rate=0.7)
+        test_discount.save()
         test_manufacurer = Manufacturer(name="Great Sports")
+        test_manufacurer.save()
         test_product = Product(name="Test", description="A description",
                                image_url="", category=test_category,
                                manufacturer=test_manufacurer,
                                quantity=10, prize=100,
                                discount_rate=test_discount,
-                               units_sold=1,
-                               id=1)
-        print(f"/products/{test_product.id}")
-        """self.assertEquals(self.client.get(f"/products/{test_product.id}")
-                          .status_code, 200)"""
-        print(test_product.id)
-        print("here")
-        """self.assertTemplateUsed(
-            self.client.get(reverse("product_page", args=[1])),
-            "products/product_page.html")"""
+                               units_sold=1,)
+        test_product.save()
+        self.assertEquals(self.client.get(reverse(
+            "product_page", kwargs={"id": test_product.id})).status_code, 200)
+        self.assertEquals(self.client.get("/products/1").status_code, 200)
+
+        self.assertTemplateUsed(
+            self.client.get(reverse(
+                "product_page", kwargs={"id": test_product.id})),
+            "products/product_page.html")

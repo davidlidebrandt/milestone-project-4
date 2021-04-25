0eaad8df22df2bd936625c043587bcea6df7ae77
davidlidebrandt <david.lidebrandt@gmail.com>
2021-04-25 11:48:02 +0000

    Update email confirmation message

--- payment/views.py
+++ payment/views.py
@@ -131,18 +131,18 @@ def confirmation_of_payment(request):
         product_list = event["data"]["object"]["metadata"]
         product_list.pop("user_id")
 
-        message = (f"Hello {order_name} Your order {order.id} was successful" +
+        message = (f"Hi {order_name} Your order {order.id} was successfull" +
                    "below you will find the details of your order." +
-                   f"total cost: {order_total}")
+                   f"\ntotal cost: {order_total}")
 
         for key, value in product_list.items():
             product = get_object_or_404(Product, id=key)
             order = get_object_or_404(Order, id=order_id)
             new_item = OrderItem(product=product, order=order, quantity=value)
-            message += f"Product: {product.name}, quantity: {value}"
+            message += f"\nProduct: {product.name}, quantity: {value}"
             new_item.save()
 
-        message += "Questions? contact us at fitness.equipment.fe@gmail.com"
+        message += "\nQuestions? contact us at fitness.equipment.fe@gmail.com"
 
         send_mail(
             "Your order",
